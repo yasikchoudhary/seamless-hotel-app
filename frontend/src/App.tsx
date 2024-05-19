8d7bc45ad2a644033f3a53798b3e57639da80c91
unknown <2020010017388@gndu.ac.in>
2024-05-19 18:40:26 +0530

    added my hotels view and added e2e test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import Register from "./pages/Register";
 import SignIn from "./pages/SignIn";
 import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
+import MyHotels from "./pages/MyHotels";
 
 const App = ()=>{
   const {isLoggedIn} = useAppContext();
@@ -34,6 +35,14 @@ const App = ()=>{
           element= {
           <Layout> 
             <AddHotel /> 
+            </Layout>
+            }
+            />
+            <Route
+         path="/my-hotels"
+          element= {
+          <Layout> 
+            <MyHotels /> 
             </Layout>
             }
             />
--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,7 +1,7 @@
 
 import {RegisterFormData} from "./pages/Register";
 import { SignInFormData } from "./pages/SignIn";
-
+import {HotelType} from "../../backend/src/shared/types"
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";    //holds the base URL of the API endpoint, allowing for 
                                                           //flexibility and easy configuration across environments.
 
@@ -89,4 +89,18 @@ export const addMyHotel = async(hotelFormData: FormData)=>{
 
        return response.json();
 }
+
+export const fetchMyHotels = async () : Promise<HotelType[]> => {
+
+    const response  = await fetch(`${API_BASE_URL}/api/my-hotels`,{
+        credentials:"include"
+    });
+
+    if(!response.ok){
+        throw new Error("Error fetching hotels");
+    }
+
+    return response.json();
+    
+} 
 
